bee6a732355c67475f249cdd576a8d815690348b
Sabyasachi Gupta <sgupta@zaloni.com>
2018-02-02 14:35:19 -0500

    initial hackathon comment

---
+++ src/main/java/com/zaloni/hackathon/zaloniautometadata/ZaloniAutometadataApplication.java
@@ -0,0 +1,12 @@
+package com.zaloni.hackathon.zaloniautometadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaloniAutometadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaloniAutometadataApplication.class, args);
+	}
+}
